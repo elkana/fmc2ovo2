e6c94a07eab3041982008eb3ada529720f89c6fb
elkana <elkana911@yahoo.com>
2018-09-17 14:09:11 +0700

    first-commit

---
+++ src/main/java/com/ppu/fmc/Fmc2ovo2Application.java
@@ -0,0 +1,12 @@
+package com.ppu.fmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fmc2ovo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fmc2ovo2Application.class, args);
+	}
+}
